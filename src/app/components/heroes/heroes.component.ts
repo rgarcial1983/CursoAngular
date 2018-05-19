96e12fddd320a41e019cc46d142a7b4518a8b3b9
Rafael Garcia <rafa.garcia.leon@gmail.com>
2018-05-19 18:05:02 +0200

    four day class

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html'
+})
+export class HeroesComponent implements OnInit {
+
+    heroes:Heroe[] = [];
+  
+  constructor( private _heroesService:HeroesService ) { 
+    console.log("Constructor");
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit");
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes);
+  }
+
+}
